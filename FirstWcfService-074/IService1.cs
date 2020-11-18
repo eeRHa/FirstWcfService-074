da4979741fd9362098e1949a388454bd1270c4ed
eeRha <arrasyidtz3@gmail.com>
2020-11-18 11:05:22 +0700

    Add project files.

---
+++ FirstWcfService-074/IService1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FirstWcfService_074
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double add(double num1, double num2);
+
+        [OperationContract]
+        double sub(double num1, double num2);
+
+        [OperationContract]
+        double multiple(double num1, double num2);
+
+        [OperationContract]
+        double divided(double num1, double num2);
+
+    }
+
+
+    
+}
